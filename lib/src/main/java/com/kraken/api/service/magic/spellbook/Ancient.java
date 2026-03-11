938ca96e94b9a30a711be1d063f7da1f4faf7e86
cbartram <cbartram3@gmail.com>
2026-03-11 17:37:08 -0400

    chore: Update the spellbook indices to account for the new Minigame teleport across all spellbooks

--- lib/src/main/java/com/kraken/api/service/magic/spellbook/Ancient.java
+++ lib/src/main/java/com/kraken/api/service/magic/spellbook/Ancient.java
@@ -16,29 +16,36 @@ public enum Ancient implements CastableSpell {
             Map.of()
     ),
 
+    MINIGAME_TELEPORT(
+            0,
+            InterfaceID.MagicSpellbook.TELEPORT_MINIGAME_STANDARD,
+            1,
+            Map.of()
+    ),
+
     // Rush Spells
     SMOKE_RUSH(
             50,
             InterfaceID.MagicSpellbook.SMOKE_RUSH,
-            1,
+            2,
             Map.of(Rune.AIR, 1, Rune.FIRE, 1, Rune.CHAOS, 2, Rune.DEATH, 2)
     ),
     SHADOW_RUSH(
             52,
             InterfaceID.MagicSpellbook.SHADOW_RUSH,
-            2,
+            3,
             Map.of(Rune.AIR, 1, Rune.CHAOS, 2, Rune.DEATH, 2, Rune.SOUL, 1)
     ),
     BLOOD_RUSH(
             56,
             InterfaceID.MagicSpellbook.BLOOD_RUSH,
-            4,
+            5,
             Map.of(Rune.CHAOS, 2, Rune.DEATH, 2, Rune.BLOOD, 1)
     ),
     ICE_RUSH(
             58,
             InterfaceID.MagicSpellbook.ICE_RUSH,
-            5,
+            6,
             Map.of(Rune.WATER, 2, Rune.CHAOS, 2, Rune.DEATH, 2)
     ),
 
@@ -46,25 +53,25 @@ public enum Ancient implements CastableSpell {
     SMOKE_BURST(
             62,
             InterfaceID.MagicSpellbook.SMOKE_BURST,
-            7,
+            8,
             Map.of(Rune.AIR, 2, Rune.FIRE, 2, Rune.CHAOS, 4, Rune.DEATH, 2)
     ),
     SHADOW_BURST(
             64,
             InterfaceID.MagicSpellbook.SHADOW_BURST,
-            8,
+            9,
             Map.of(Rune.AIR, 1, Rune.CHAOS, 4, Rune.DEATH, 2, Rune.SOUL, 2)
     ),
     BLOOD_BURST(
             68,
             InterfaceID.MagicSpellbook.BLOOD_BURST,
-            10,
+            11,
             Map.of(Rune.CHAOS, 2, Rune.DEATH, 4, Rune.BLOOD, 2)
     ),
     ICE_BURST(
             70,
             InterfaceID.MagicSpellbook.ICE_BURST,
-            11,
+            12,
             Map.of(Rune.WATER, 4, Rune.CHAOS, 4, Rune.DEATH, 2)
     ),
 
@@ -72,25 +79,25 @@ public enum Ancient implements CastableSpell {
     SMOKE_BLITZ(
             74,
             InterfaceID.MagicSpellbook.SMOKE_BLITZ,
-            13,
+            14,
             Map.of(Rune.AIR, 2, Rune.FIRE, 2, Rune.DEATH, 2, Rune.BLOOD, 2)
     ),
     SHADOW_BLITZ(
             76,
             InterfaceID.MagicSpellbook.SHADOW_BLITZ,
-            14,
+            15,
             Map.of(Rune.AIR, 2, Rune.DEATH, 2, Rune.BLOOD, 2, Rune.SOUL, 2)
     ),
     BLOOD_BLITZ(
             80,
             InterfaceID.MagicSpellbook.BLOOD_BLITZ,
-            16,
+            17,
             Map.of(Rune.DEATH, 2, Rune.BLOOD, 4)
     ),
     ICE_BLITZ(
             82,
             InterfaceID.MagicSpellbook.ICE_BLITZ,
-            17,
+            18,
             Map.of(Rune.WATER, 3, Rune.DEATH, 2, Rune.BLOOD, 2)
     ),
 
@@ -98,25 +105,25 @@ public enum Ancient implements CastableSpell {
     SMOKE_BARRAGE(
             86,
             InterfaceID.MagicSpellbook.SMOKE_BARRAGE,
-            20,
+            21,
             Map.of(Rune.AIR, 4, Rune.FIRE, 4, Rune.DEATH, 4, Rune.BLOOD, 2)
     ),
     SHADOW_BARRAGE(
             88,
             InterfaceID.MagicSpellbook.SHADOW_BARRAGE,
-            21,
+            22,
             Map.of(Rune.AIR, 4, Rune.DEATH, 4, Rune.BLOOD, 2, Rune.SOUL, 3)
     ),
     BLOOD_BARRAGE(
             92,
             InterfaceID.MagicSpellbook.BLOOD_BARRAGE,
-            23,
+            24,
             Map.of(Rune.DEATH, 4, Rune.BLOOD, 4, Rune.SOUL, 1)
     ),
     ICE_BARRAGE(
             94,
             InterfaceID.MagicSpellbook.ICE_BARRAGE,
-            24,
+            25,
             Map.of(Rune.WATER, 6, Rune.DEATH, 4, Rune.BLOOD, 2)
     ),
 
@@ -124,19 +131,19 @@ public enum Ancient implements CastableSpell {
     PADDEWWA_TELEPORT(
             54,
             InterfaceID.MagicSpellbook.ZAROSTELEPORT1,
-            3,
+            4,
             Map.of(Rune.AIR, 1, Rune.FIRE, 1, Rune.LAW, 2)
     ),
     SENNTISTEN_TELEPORT(
             60,
             InterfaceID.MagicSpellbook.ZAROSTELEPORT2,
-            6,
+            7,
             Map.of(Rune.LAW, 2, Rune.SOUL, 1)
     ),
     GHORROCK_TELEPORT(
             96,
             InterfaceID.MagicSpellbook.ZAROSTELEPORT8,
-            25,
+            26,
             Map.of(Rune.WATER, 8, Rune.LAW, 2)
     );
 
