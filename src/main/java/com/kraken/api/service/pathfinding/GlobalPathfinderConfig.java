ae55862ffc557bdb115d28f8434b3c1d54d6f5bf
cbartram <cbartram3@gmail.com>
2026-03-13 14:32:38 -0400

    refactor: Temporarily remove global pathfinding

---
+++ src/main/java/com/kraken/api/service/pathfinding/GlobalPathfinderConfig.java
@@ -0,0 +1,84 @@
+package com.kraken.api.service.pathfinding;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@Builder(toBuilder = true)
+public class GlobalPathfinderConfig {
+
+    private final long calculationCutoffMillis;
+
+    @Builder.Default private final boolean avoidWilderness = false;
+
+    @Builder.Default
+    private final boolean useAgilityShortcuts      = true;
+
+    @Builder.Default
+    private final boolean useGrappleShortcuts      = true;
+
+    @Builder.Default
+    private final boolean useBoats                 = true;
+
+    @Builder.Default
+    private final boolean useCanoes                = true;
+
+    @Builder.Default
+    private final boolean useCharterShips          = true;
+
+    @Builder.Default
+    private final boolean useShips                 = true;
+
+    @Builder.Default
+    private final boolean useFairyRings            = true;
+
+    @Builder.Default
+    private final boolean useGnomeGliders          = true;
+
+    @Builder.Default
+    private final boolean useHotAirBalloons        = true;
+
+    @Builder.Default
+    private final boolean useMagicCarpets          = true;
+
+    @Builder.Default
+    private final boolean useMagicMushtrees        = true;
+
+    @Builder.Default
+    private final boolean useMinecarts             = true;
+
+    @Builder.Default
+    private final boolean useQuetzals              = true;
+
+    @Builder.Default
+    private final boolean useSeasonalTransports    = true;
+
+    @Builder.Default
+    private final boolean useSpiritTrees           = true;
+
+    @Builder.Default
+    private final boolean useTeleportationItems    = true;
+
+    @Builder.Default
+    private final boolean useTeleportationBoxes    = true;
+
+    @Builder.Default
+    private final boolean useTeleportationLevers   = true;
+
+    @Builder.Default
+    private final boolean useTeleportationMinigames = true;
+
+    @Builder.Default
+    private final boolean useTeleportationPortals  = true;
+
+    @Builder.Default
+    private final boolean useTeleportationPortalsPoh = true;
+
+    @Builder.Default
+    private final boolean useTeleportationSpells   = true;
+
+    @Builder.Default
+    private final boolean useWildernessObelisks    = true;
+}
