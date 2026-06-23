127a52bef22342897acf2f55446e888fb664d05f
cbartram <cbartram3@gmail.com>
2026-06-23 12:38:05 -0400

    feat: Add static and arg count checks to platform info hooks

--- src/main/java/com/kraken/api/core/hooks/SecurityHooks.java
+++ src/main/java/com/kraken/api/core/hooks/SecurityHooks.java
@@ -14,5 +14,7 @@ public class SecurityHooks {
     String platformInfoMethodName;
     String callStackField;
     String agentField;
+    boolean isPlatformInfoMethodStatic;
+    int platformInfoMethodArgCount;
 }
 
